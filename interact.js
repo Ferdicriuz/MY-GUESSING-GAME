7c6eb439bcf1d3424237f0b35aebe41bd387a32c
ferdicriuz <lderrickwealth@gmail.com>
2025-01-06 14:09:09 +0100

    added more structure

--- interact.js
+++ interact.js
@@ -1,1 +1,8 @@
 "use strict"
+
+let number = Math.trunc(Math.random() * 20 + 1);
+const realNum = document.querySelector(".number");
+
+document.querySelector(".check").addEventListener("click", function(){
+    const guess = Number(document.querySelector(".guess").value);
+})
